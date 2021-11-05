f67547744f907bc370ec78c3c3158e2e94c2950b
KPMGE <jesuskevin254@gmail.com>
2021-11-05 10:03:47 -0300

    Fixing cors erros

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 
 app.use(express.json());
 
